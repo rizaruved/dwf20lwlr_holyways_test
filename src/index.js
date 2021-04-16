212414d9a578877a4d1229d3f1e0dfcabfdf579b
Shaprizal Ibrahim <shaprizalibrahim@gmail.com>
2021-04-16 19:56:22 +0700

    / ‘src/index.js’

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Home from "./pages/Home";
 
 ReactDOM.render(
   <>
     <App />
+    <Home />
   </>,
   document.getElementById("root")
 );

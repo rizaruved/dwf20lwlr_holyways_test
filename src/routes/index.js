96caa72a71b9d3c588123786baaceacc29a3ab72
Shaprizal Ibrahim <shaprizalibrahim@gmail.com>
2021-04-18 15:55:02 +0700

    / ‘src/routes/index.js’

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,6 +14,7 @@ import Navbar from "../components/Navbar";
 const Routes = () => {
   return (
     <div>
+      <Navbar />
       <Switch>
         <Route path="/" exact component={Homepage} />
         <Route path="/profile" exact component={Profile} />

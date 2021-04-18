a6fb880df50a132c33a791821ccd4971a4033a45
Shaprizal Ibrahim <shaprizalibrahim@gmail.com>
2021-04-18 14:29:20 +0700

    / ‘src/routes/index.js’

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+import Homepage from "../pages/Homepage";
+import Profile from "../pages/Profile";
+import RaiseFund from "../pages/RaiseFund";
+import FormFund from "../pages/FormFund";
+import DonationDetail from "../pages/DonationDetail";

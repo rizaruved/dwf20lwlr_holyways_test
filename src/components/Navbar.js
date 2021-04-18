b3f9d356d7690162c42d4d02c09470c11c1f6296
Shaprizal Ibrahim <shaprizalibrahim@gmail.com>
2021-04-18 15:59:20 +0700

    / ‘src/components/Navbar.js’

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,32 @@
-ulimport React from 'React';
-im<li><Link to="/">Home</Link></li>port { Link } from 'react-router-dom';
+import React from "React";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <ul>
-           <li><Link to="/">Home</Link></li>
-           <li><Link to="/profile">Profile</Link></li>
-           <li><Link to="/raise-fund">Campaign</Link></li>
-           <li><Link to="/form-fund">Form</Link></li>
-           <li><Link to="/donation-detail">Donation</Link></li>
-           <li><Link to="/view-fund">View</Link></li>
-           <li><Link to="">Error</Link></li>
-        </ul>
-    )
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/profile">Profile</Link>
+      </li>
+      <li>
+        <Link to="/raise-fund">Campaign</Link>
+      </li>
+      <li>
+        <Link to="/form-fund">Form</Link>
+      </li>
+      <li>
+        <Link to="/donation-detail">Donation</Link>
+      </li>
+      <li>
+        <Link to="/view-fund">View</Link>
+      </li>
+      <li>
+        <Link to="">Error</Link>
+      </li>
+    </ul>
+  );
 }
 
-export default Navbar
+export default Navbar;

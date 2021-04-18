9a30df55c66752435ea07c58cd097255721b5d4f
Shaprizal Ibrahim <shaprizalibrahim@gmail.com>
2021-04-18 13:59:47 +0700

    - ‘src/App.js’ + ‘src/components/’ - ‘src/components/Navbar.js’ + ‘src/pages/’ - ‘src/pages/Home.js’

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import Home from "./pages/Home";
-
-function App() {
-  return <h1>This is a react app.</h1>;
-  <Home />;
-}
-
-export default App;
--- src/components/Navbar.js
+++
@@ -1,8 +0,0 @@
-function Navbar() {
-  return;
-  <div>
-    <h3>Ini navbar</h3>
-  </div>;
-}
-
-export default Navbar;

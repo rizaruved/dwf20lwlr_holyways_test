fe5e734f63d9a1078af8b60a4119cd0c2dac15e0
Shaprizal Ibrahim <shaprizalibrahim@gmail.com>
2021-04-18 21:21:43 +0700

    / ‘src/components/Navbar.js’

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,34 @@
 import { Link } from "react-router-dom";
 import Styles from "./styles/navbar.module.css";
-import from "react"
+import Logo from "../assets/holyways-icon.svg";
 
 function Navbar() {
   return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/profile">Profile</Link>
-      </li>
-      <li>
-        <Link to="/raise-fund">Campaign</Link>
-      </li>
-      <li>
-        <Link to="/form-fund">Form</Link>
-      </li>
-      <li>
-        <Link to="/donation-detail">Donation</Link>
-      </li>
-      <li>
-        <Link to="/view-fund">View</Link>
-      </li>
-      <li>
-        <Link to="/error">Error</Link>
-      </li>
-    </ul>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/raise-fund">Campaign</Link>
+        </li>
+        <li>
+          <Link to="/form-fund">Form</Link>
+        </li>
+        <li>
+          <Link to="/donation-detail">Donation</Link>
+        </li>
+        <li>
+          <Link to="/view-fund">View</Link>
+        </li>
+        <li>
+          <Link to="/error">Error</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
 
